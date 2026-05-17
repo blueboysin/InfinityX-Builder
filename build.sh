4a822e716dced3298ed7583e70be803d04cd105d
blueboysin <68513793+blueboysin@users.noreply.github.com>
2026-05-17 10:18:38 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Prepare clean building folders
+mkdir -p ~/android
+cd ~/android
+
+# Configure Git identities
+git config --global user.name "blueboysin"
+git config --global user.email "blueboysin@github.com"
+
+# Pull Project Infinity-X 3.10 Android 16 code base
+repo init -u https://github.com/projectinfinity-x/manifest.git -b 16 --git-lfs --depth=1 --no-repo-verify
+
+# Copy your custom munch map into the main system
+mkdir -p .repo/local_manifests
+cp /tmp/munch.xml .repo/local_manifests/munch.xml
+
+# Download everything cleanly
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --depth=1
+
+# Set up terminal compilation commands
+. build/envsetup.sh
+
+# Select POCO F4 as our target phone
+lunch infinity_munch-userdebug
+
+# Bake the ROM!
+mka bacon -j$(nproc --all)
